a276bd6e026e308a4eaad5ada3d8a7be38c1e7b5
Ilya_Myasnichenko <129312680+Ilya-m2195@users.noreply.github.com>
2023-08-22 18:44:01 +0600

    fixed errors

--- JS/index.js
+++ JS/index.js
@@ -35,33 +35,6 @@ close.addEventListener('click', function() {
   headerOpenBtn.classList.remove('header__search-open-btn--active');
 });
 
-  /* header navigation links */
-
-  const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-    animationTime = 250,
-    framesCount = 25;
-
-  anchors.forEach(function (item) {
-    item.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      let coordY = document.querySelector(item.getAttribute('#href')).getBoundingClientRect().top + window.pageYOffset;
-
-      let scroller = setInterval(function () {
-        let scrollBy = coordY / framesCount;
-
-        if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-
-          window.scrollBy(0, scrollBy);
-        } else {
-
-          window.scrollTo(0, coordY);
-          clearInterval(scroller);
-        }
-      }, animationTime / framesCount);
-    });
-  });
-
   /* menu */
 
   const menu = document.querySelector('.menu');
